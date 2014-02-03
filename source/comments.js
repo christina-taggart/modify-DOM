a905aa920ee66345e869a6e69ff155dc34479be1
Alexander Leishman & John Olmsted <pair+leishman.qsymmachus@devbootcamp.com>
2014-02-03 14:15:55 -0800

    Comment box appears on button click

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,15 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  // On click of new comment
+    // make button disappear
+    // Show comment form
+  $('#new_comment_button').click( function(){
+    $(this).css('display', 'none');
+    $('.comment-form').css('display', 'block');
+  });
+
+  // On click of form submit
+    // make form disappear
+    //append new comment to previous one
+    //show new comment button again
 });
