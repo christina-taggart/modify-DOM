438a38d1e5b051088e04458d8ba01a5350dc88bc
Alexander Leishman & John Olmsted <pair+leishman.qsymmachus@devbootcamp.com>
2014-02-03 14:32:19 -0800

    Add comment button appends submitted comment to comment list

--- source/comments.js
+++ source/comments.js
@@ -12,4 +12,19 @@ $(function(){
     // make form disappear
     //append new comment to previous one
     //show new comment button again
+  $('#add-comment-button').click( function(event){
+    event.preventDefault();
+    var comment = $('.comment-form > textarea').val()
+    var name = $('.comment-form > input[name=name]').val()
+    $('#comment_list').append(comment_body(comment, name));
+    $('.comment-form').css('display', 'none');
+    $('#new_comment_button').css('display', 'block');
+
+
+  });
+
+  var comment_body = function(comment, name){
+    return ('<li>' + comment + '<span class="author">' + name + '</span>' + '</li>')
+  }
+
 });
