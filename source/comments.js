cbde71e755d152ba0036dd759874b7c43b163f84
Kevin Yeh & Patrick Vilhena <pair+kyeh.nifengle@devbootcamp.com>
2014-02-03 19:00:17 -0800

    Release 0

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,20 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  $('#new_comment_button').on( "click", showCommentFields)
 });
+
+var showCommentFields = function(event){
+  var $new_comment = $('#hidden #new_comment_template li').clone()
+  $new_comment.appendTo("#comment_list");
+  event.preventDefault();
+  $("#new_comment").on( "submit",createComment)
+}
+
+var createComment = function(event){
+  event.preventDefault();
+  var $new_comment = $('#hidden #new_list_item_template li').clone();
+  $new_comment.find('.comment_body').text(this.newpost.value);
+  $new_comment.find('.author').text(this.authorName.value);
+  $new_comment.appendTo("#comment_list");
+  this.parentElement.remove();
+}
