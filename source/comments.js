4c5f69667d95dd02eafa509e42b93e61b9f5e5db
shkurkin <eshkurkin@gmail.com>
2014-02-03 17:04:53 -0800

    Add comment functionality, show and hide comment form

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,13 @@
-// Shorthand for document ready
 $(function(){
-  // Your code goes here
+  $("#new_comment").hide();
+  $("#new_comment_button").click(function(){
+    $("#new_comment").show();
+  });
+  $("#new_comment").submit(function(event){
+    event.preventDefault();
+    var comment = $("textarea[name='comment']").val();
+    var name = $("input[name='name']").val();
+    $("#comment_list").append("<li>" + comment + "<span class='author'>" + name + "</span>" + "</li>");
+    $("#new_comment").hide();
+  })
 });
