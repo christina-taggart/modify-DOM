014e5393f0b917409672e412a304ef2417a69dff
Darcey Lachtman <dslachtman@gmail.com>
2014-02-03 19:56:21 -0800

    ability to add new commments and populate them

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,16 @@
 // Shorthand for document ready
 $(function(){
   // Your code goes here
+  $("#new_comment_button").click(function(){
+  	$(".comment_form").show();
+  		$(".submit").on("click", function(event){
+  			var text = $(".comment_form > input[name=text]").val();
+  			var author = $(".comment_form > input[name=author]").val();
+  			event.preventDefault();
+  			$("#comment_list").append("<li>" + text + "<span class= 'author'>" + author);
+ 			
+  		}) 
+
+  })
 });
+
