acec1e2ce96c805ae610e1e1fcdda5deff08aa71
shkurkin <eshkurkin@gmail.com>
2014-02-03 17:49:14 -0800

    Rao's wisdom

--- source/comments.js
+++ source/comments.js
@@ -1,13 +1,23 @@
 $(function(){
   $("#new_comment").hide();
-  $("#new_comment_button").click(function(){
-    $("#new_comment").show();
-  });
-  $("#new_comment").submit(function(event){
-    event.preventDefault();
-    var comment = $("textarea[name='comment']").val();
-    var name = $("input[name='name']").val();
-    $("#comment_list").append("<li>" + comment + "<span class='author'>" + name + "</span>" + "</li>");
-    $("#new_comment").hide();
-  })
+  $("#new_comment_button").click(showCommentForm);
+  $("#new_comment").submit(addComment);
 });
+
+var showCommentForm = function(){
+  $("#new_comment").show();
+};
+
+var addComment = function(event){
+  event.preventDefault();
+
+  var comment = this["comment"].value;
+  var name = this["name"].value;
+
+  var template = $("#comment-template").clone();
+  template.find(".msg-txt").text(comment);
+  template.find(".author").text(name);
+
+  $("#comment_list").append(template);
+  $("#new_comment").hide();
+};
