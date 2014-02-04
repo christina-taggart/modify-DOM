163d1c7ef149d91b0d128083f67008a6e5bc677f
Aditya Mahesh & Spencer Smitherman <pair+amahesh0501.bigspencey@devbootcamp.com>
2014-02-03 16:54:17 -0800

    Completed Release 0

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,14 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+
+  $("#new_comment_button").on("click", function(){
+    $(".comment_form").fadeIn(300)
+    $("#new_comment_button").fadeOut(300)
+      $(".submit").on("click", function(e){
+        var text = $(".comment_form > input[name=text_field]").val()
+        var author = $(".comment_form > input[name=author_field]").val()
+        e.preventDefault()
+        $("#comment_list").append("<li>" + text + "<span class='author'>" + author + "</span></li>")
+    })
+  });
 });
