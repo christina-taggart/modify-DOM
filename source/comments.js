8e0372c8b6252301beaf1ff3e046f652faff941b
& <pair+lee-sh-roy.mh120888@devbootcamp.com>
2014-02-03 18:45:12 -0800

    Added reply form

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,17 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  $("button").on("click", function(){
+    $("<form id='new_comment'>Author:<input type='text' name='authorName' /><br/>Comment:<input type='text' name='comment' /><input type='submit' /></form>").appendTo("ul");
+  });
+
+
+  $( "#comment_area" ).on("submit", "form", function(event) {
+    event.preventDefault();
+    var form = document.querySelector("#new_comment");
+    var author = form["authorName"].value;
+    var comment = form["comment"].value;
+    $("<li>" + comment + "<span class='author'>" + author + "</span></li>").appendTo("ul");
+    $("form").css('display', 'none');
+  });
+
 });
