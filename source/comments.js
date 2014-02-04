4de953ed0131c36072129ee5e93a2f69ae96bb33
Apprentice <apprentice@dbc09.local>
2014-02-03 16:10:41 -0800

    Added posting capability to page

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,20 @@
 // Shorthand for document ready
 $(function(){
   // Your code goes here
+ $('#new_comment_button').on('click', function() {
+  $( "<form class='add-comment'><input type='text' name='content'> <input type='text' name='name'><input type='submit' value='comment!'></form>" ).appendTo( "div#comment_area" );
+  $('.add-comment').submit( function(event) {
+    event.preventDefault();
+    var content = $(this).serializeArray()[0].value
+    var name = $(this).serializeArray()[1].value
+    $("<li>" + content + "<span class='author'>" + name + "</span></li>").appendTo("ul")
+    $('.add-comment').remove()
+    event.preventDefault();
+  })
+ });
+
+
 });
+
+
+
