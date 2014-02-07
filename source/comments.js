381e43a8a6d376ef3ef08ac5bc4f3468c9f975e7
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-06 17:19:18 -0800

    Allowed you to add comments via form using jQuery

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,13 @@
-// Shorthand for document ready
-$(function(){
-  // Your code goes here
-});
+$('#new_comment_button').on('click', function(e){
+  $('#new_comment_button').hide();
+  e.preventDefault();
+  console.log('clicked')
+  $('#add_comment_form').css('display', 'inline')
+  $('#add_comment_form').on('submit', function(event){
+    event.preventDefault();
+    var comment_text = $("input[name='comment_text']").val()
+    var commenter = $("input[name='commenter_name']").val()
+    $('#comment_list').append($("<li>" + comment_text + "<span class='author'>" + commenter + "</span></li>"));
+  })
+})
+
